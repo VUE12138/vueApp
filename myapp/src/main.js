49fff918c844bbfa9fdae05f44821b793f597b52
wkp12138 <wangkp12138@163.com>
2019-03-06 16:48:08 +0800

    Update main.js

--- myapp/src/main.js
+++ myapp/src/main.js
@@ -9,6 +9,18 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 Vue.prototype.$http = axios
 
+//设置axios拦截器，对所有请求在token存在的情况下，在请求头携带token
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+	  if (store.getters.token) {
+	    config.headers['token'] = store.getters.token
+	  }
+	    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+
 Vue.config.productionTip = false
 new Vue({
   el: '#app',

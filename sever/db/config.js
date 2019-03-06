03233c2fee87b2dd9f4db9947bb7e87b92fd9e26
wkp12138 <wangkp12138@163.com>
2019-03-06 10:20:25 +0800

    db had changed ,10.88.1.126

--- sever/db/config.js
+++ sever/db/config.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({   
-      host: 'localhost',  // 主机名
-      user: 'root',       // 用户名
-      password: '123',       // 密码
+      host: '10.88.1.126',  // 主机名
+      user: 'wkp',       // 用户名
+      password: '1234',       // 密码
       database: 'VueApp',       // 数据库名 
       port: 3306          // 端口号（默认都是3306）
   });

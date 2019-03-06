4868dfe4cdd909ea238a596d076b00a48e068a7c
wkp12138 <wangkp12138@163.com>
2019-03-06 16:41:38 +0800

    Update store.js

---
+++ myapp/src/store/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+export default new Vuex.store({
+	state:{
+		token:''
+	},
+	mutations:{
+		set_token(state,token){
+			state.token = token
+			sessionStorage.token = token
+		},
+		del_token(state){
+			state.token = ''
+			sessionStorage.removeItem('token')
+		}
+	}
+})
